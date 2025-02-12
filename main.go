fa66c854308d8936cc0131eba5c8d5fccb7a1b66
Dav He <s991179@gmail.com>
2025-02-12 10:22:44 +0800

    feat: test field validation

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import "github.com/go-playground/validator/v10"
+
+type User struct {
+	Username string `validate:"required,min=3,max=20"`
+	Email    string `validate:"required,email"`
+	Age      int    `validate:"gte=18,lte=60"` // Age must be between 18-60
+}
+
 func main() {
-	print("Test")
+	validate := validator.New()
+
+	user := User{
+		Username: "GoDev",
+		Email:    "invalid-email",
+		Age:      17,
+	}
+
+	errs := validate.Struct(user)
+
+	if errs != nil {
+		for _, err := range errs.(validator.ValidationErrors) {
+			print("Field: ", err.Field(), ", Error: ", err.Tag())
+			println("")
+		}
+	} else {
+		print("Success")
+	}
+
 }
